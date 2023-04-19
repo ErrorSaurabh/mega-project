1e75ba758b0c907d34e4d2d96b1c898bd23919f1
ErrorSaurabh <saurabhchauragade69@gmail.com>
2023-04-19 16:33:55 +0530

    pagelimit

--- controllers/productController.js
+++ controllers/productController.js
@@ -25,37 +25,38 @@ export const createProduct = asyncHandler(async (req, res) => {
   });
 });
 
-// export const getProducts = asyncHandler(async (req, res) => {
-//   try {
-//     const nameFilter = req.query.name ? { name: { $regex: req.query.name, $options: 'i' } } : {};
-//     const products = await Product.find(nameFilter);
-
-//     if (products.length === 0) {
-//       return res.status(404).json({ message: 'No data found' });
-//     }
-
-//     res.status(200).json(products);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
 // Get all products with optional name filter
 export const getProducts = asyncHandler(async (req, res) => {
   try {
-    const { name, brand, color, size } = req.query;
+    const { name, brand, color, size, priceRange, page, limit } = req.query;
 
     const nameFilter = name ? { name: { $regex: name, $options: 'i' } } : {};
     const brandFilter = brand ? { brand: { $regex: brand, $options: 'i' } } : {};
     const colorFilter = color ? { color: { $regex: color, $options: 'i' } } : {};
     const sizeFilter = size ? { size: { $regex: size, $options: 'i' } } : {};
 
+    let priceFilter = {};
+    if (priceRange) {
+      const priceRanges = priceRange.split("-");
+      const minPrice = parseInt(priceRanges[0]);
+      const maxPrice = parseInt(priceRanges[1]);
+      priceFilter = { price: { $gte: minPrice, $lte: maxPrice } };
+    }
+
+    const pageNumber = parseInt(page) || 1;
+    const limitNumber = parseInt(limit) || 5;
+
+    const skip = (pageNumber - 1) * limitNumber;
+
     const products = await Product.find({
       ...nameFilter,
       ...brandFilter,
       ...colorFilter,
       ...sizeFilter,
-    });
+      ...priceFilter,
+    })
+      .skip(skip)
+      .limit(limitNumber);
 
     if (products.length === 0) {
       return res.status(404).json({ message: "No products found" });
@@ -66,8 +67,6 @@ export const getProducts = asyncHandler(async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-
-
 
 // Get a product by id
 export const getProductById = asyncHandler(async (req, res) => {
